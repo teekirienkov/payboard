13e18f251c921c121694e98dfe1da3c88368f762
Timur <teekirienkov@gmail.com>
2020-06-05 18:25:12 +0300

    add renderNewPost module and sendRequest method in class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import Service from './service';
 import renderPost from './renderPost';
+import renderNewPost from './renderNewPost';
 
 const createPostInput = document.querySelector('.create-post-input'),
       buttonPost = document.querySelector('.button-post'),
       mainFeed = document.querySelector('.main-feed'),
       onePost = document.querySelector('.post');
 
+buttonPost.addEventListener('click', (event) => {
+  renderNewPost(createPostInput.textContent)
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   const response = new Service();
   response.getData('db/posts.json')
-    .then(response => renderPost(response))
-})
+    .then(response => renderPost(response));
+});
 
--- src/service.js
+++ src/service.js
@@ -1,11 +1,33 @@
 class Service {
-  async getData (url) {
+  async getData(url) {
     const response = await fetch(url);
     if (response.ok) {
       return response.json();
     } else {
       throw new Error(`Error: ${response.status}`)
     }
+  }
+
+  sendRequest (method, url, body = null) {
+    const headersNull = {
+      'Content-Type': 'application/json'
+    }
+    return fetch(url, { 
+      method: method, 						
+      body: JSON.stringify(body),
+      headers: headersNull
+    })
+      .then(response=>{
+        if (response.ok) {
+          return response.json()
+        }
+        
+        return response.json().then(error => {
+          const e = new Error('Что-то пошло не так'); 
+          e.data = error;
+          throw e
+        })
+      })
   }
 }
 
